3a8a84024998c546756843a0ea52963e1a793ae7
clay-most <46730958+clay-most@users.noreply.github.com>
2019-04-19 15:27:31 -0600

    counter

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,11 +15,11 @@ dad();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function () {count = count + 1; console.log(count); return count;}
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+counter();
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
